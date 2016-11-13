3c3937d3e66850ab073071135d5466f61229a63c
Benjamin Eidelman <beneidel@gmail.com>
2016-11-12 21:22:58 -0300

    fixed logging tests

--- test/middleware.js
+++ test/middleware.js
@@ -58,13 +58,21 @@ describe('fetchWrap/middleware', function() {
   });
 
   describe('#logger', function() {
+    function serialize() {
+      return Array.prototype.slice.call(arguments).map(function(item) {
+        if (typeof item === 'object') {
+          return JSON.stringify(item);
+        }
+        return item;
+      }).join(' ');
+    }
     it('logs request and success', function() {
       const mockedFetch = mockFetch(123);
       const output = [];
       const fetch = fetchWrap(mockedFetch, [
         middleware.logger({
           success: true,
-          log: function(msg) { output.push(Array.prototype.join.call(arguments, ' ')); }
+          log: function() { output.push(serialize.apply(this, arguments)); }
         })
       ]);
       return fetch('http://localhost/fake-url').then(function(result) {
@@ -79,7 +87,7 @@ describe('fetchWrap/middleware', function() {
       const output = [];
       const fetch = fetchWrap(mockedFetch, [
         middleware.logger({
-          log: function(msg) { output.push(Array.prototype.join.call(arguments, ' ')); }
+          log: function() { output.push(serialize.apply(this, arguments)); }
         })
       ]);
       return fetch('http://localhost/fake-url').catch(function(err) {
@@ -88,7 +96,7 @@ describe('fetchWrap/middleware', function() {
       }).then(function(result) {
         expect(output).to.eql([
           'debug [fetch] GET http://localhost/fake-url start',
-          'error [fetch] GET http://localhost/fake-url failed Error: request failed'
+          'error [fetch] GET http://localhost/fake-url failed {"message":"request failed"}'
         ]);
       });
     });
@@ -102,7 +110,7 @@ describe('fetchWrap/middleware', function() {
       const output = [];
       const fetch = fetchWrap(mockedFetch, [
         middleware.logger({
-          log: function(msg) { output.push(Array.prototype.join.call(arguments, ' ')); }
+          log: function() { output.push(serialize.apply(this, arguments)); }
         })
       ]);
       return fetch('http://localhost/fake-url', {
@@ -129,7 +137,7 @@ describe('fetchWrap/middleware', function() {
       const fetch = fetchWrap(mockedFetch, [
         middleware.logger({
           success: true,
-          log: function(msg) { output.push(Array.prototype.join.call(arguments, ' ')); }
+          log: function() { output.push(serialize.apply(this, arguments)); }
         })
       ]);
       return fetch('http://localhost/fake-url', {
@@ -156,7 +164,7 @@ describe('fetchWrap/middleware', function() {
       const fetch = fetchWrap(mockedFetch, [
         middleware.logger({
           success: true,
-          log: function(msg) { output.push(Array.prototype.join.call(arguments, ' ')); }
+          log: function() { output.push(serialize.apply(this, arguments)); }
         })
       ]);
       return expect(fetch('http://localhost/fake-url', {
@@ -168,176 +176,176 @@ describe('fetchWrap/middleware', function() {
         expect(output).to.eql([
           'debug [fetch] GET http://localhost/fake-url start',
           'warn [fetch] GET http://localhost/fake-url timeout 0.1',
-          'error [fetch] GET http://localhost/fake-url failed Error: request failed'
+          'error [fetch] GET http://localhost/fake-url failed {"message":"request failed"}'
         ]);
       });
     });
+  });
 
-    describe('#urlParams', function() {
-      it('fills url tokens', function() {
-        const mockedFetch = mockFetch(123);
-        const fetch = fetchWrap(mockedFetch, [
-          middleware.urlParams({
-            id: 57,
-            section: 'settings'
-          })
-        ]);
-        return fetch('http://localhost/users/{id}/{section}').then(function(result) {
-          expect(mockedFetch.calls[0].url).to.eql(
-            'http://localhost/users/57/settings'
-          );
-        });
-      });
-      it('fills with additional params', function() {
-        const mockedFetch = mockFetch(123);
-        const fetch = fetchWrap(mockedFetch, [
-          middleware.urlParams({
-            id: 57
-          })
-        ]);
-        return fetch('http://localhost/users/{id}/{section}', {
-          params: {
-            section: 'notifications',
-            token: 1234,
-            'email': 'john@smith.com'
-          }
-        }).then(function(result) {
-          expect(mockedFetch.calls[0].url).to.eql(
-            'http://localhost/users/57/notifications?token=1234&email=john%40smith.com'
-          );
-        });
-      });
-      it('fails if token couldn\'t be replaced', function() {
-        const mockedFetch = mockFetch(123);
-        const fetch = fetchWrap(mockedFetch, [
-          middleware.urlParams({
-            id: 57
-          })
-        ]);
-        return expect(fetch('http://localhost/users/{id}/{section}', {
-          params: {
-            token: 1234,
-            'email': 'john@smith.com'
-          }
-        })).to.eventually.be.rejectedWith('url param not found: {section}');
-      });
-      it('fail is missing can be disabled', function() {
-        const mockedFetch = mockFetch(123);
-        const fetch = fetchWrap(mockedFetch, [
-          middleware.urlParams({
-            id: 57
-          }, false)
-        ]);
-        return expect(fetch('http://localhost/users/{id}/{section}', {
-          params: {
-            token: 1234,
-            'email': 'john@smith.com'
-          }
-        })).to.eventually.eql(123);
+  describe('#urlParams', function() {
+    it('fills url tokens', function() {
+      const mockedFetch = mockFetch(123);
+      const fetch = fetchWrap(mockedFetch, [
+        middleware.urlParams({
+          id: 57,
+          section: 'settings'
+        })
+      ]);
+      return fetch('http://localhost/users/{id}/{section}').then(function(result) {
+        expect(mockedFetch.calls[0].url).to.eql(
+          'http://localhost/users/57/settings'
+        );
       });
     });
+    it('fills with additional params', function() {
+      const mockedFetch = mockFetch(123);
+      const fetch = fetchWrap(mockedFetch, [
+        middleware.urlParams({
+          id: 57
+        })
+      ]);
+      return fetch('http://localhost/users/{id}/{section}', {
+        params: {
+          section: 'notifications',
+          token: 1234,
+          'email': 'john@smith.com'
+        }
+      }).then(function(result) {
+        expect(mockedFetch.calls[0].url).to.eql(
+          'http://localhost/users/57/notifications?token=1234&email=john%40smith.com'
+        );
+      });
+    });
+    it('fails if token couldn\'t be replaced', function() {
+      const mockedFetch = mockFetch(123);
+      const fetch = fetchWrap(mockedFetch, [
+        middleware.urlParams({
+          id: 57
+        })
+      ]);
+      return expect(fetch('http://localhost/users/{id}/{section}', {
+        params: {
+          token: 1234,
+          'email': 'john@smith.com'
+        }
+      })).to.eventually.be.rejectedWith('url param not found: {section}');
+    });
+    it('fail is missing can be disabled', function() {
+      const mockedFetch = mockFetch(123);
+      const fetch = fetchWrap(mockedFetch, [
+        middleware.urlParams({
+          id: 57
+        }, false)
+      ]);
+      return expect(fetch('http://localhost/users/{id}/{section}', {
+        params: {
+          token: 1234,
+          'email': 'john@smith.com'
+        }
+      })).to.eventually.eql(123);
+    });
+  });
 
-    describe('#sendJSON', function() {
-      it('sends body as JSON', function() {
-        const mockedFetch = mockFetch(123);
-        const fetch = fetchWrap(mockedFetch, [
-          middleware.sendJSON()
-        ]);
-        return fetch('http://localhost/users', {
+  describe('#sendJSON', function() {
+    it('sends body as JSON', function() {
+      const mockedFetch = mockFetch(123);
+      const fetch = fetchWrap(mockedFetch, [
+        middleware.sendJSON()
+      ]);
+      return fetch('http://localhost/users', {
+        method: 'POST',
+        body: {
+          name: 'john'
+        }
+      }).then(function() {
+        expect(mockedFetch.calls[0].options).to.eql({
           method: 'POST',
-          body: {
-            name: 'john'
-          }
-        }).then(function() {
-          expect(mockedFetch.calls[0].options).to.eql({
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: '{"name":"john"}'
-          });
-        });
-      });
-    });
-
-    describe('#receiveJSON', function() {
-      it('receives JSON responses', function() {
-        const mockedFetch = mockFetch({
-          ok: true,
           headers: {
-            get: function(name) {
-              if (name === 'content-type') {
-                return 'application/json';
-              }
-            }
+            'Content-Type': 'application/json'
           },
-          text: function() {
-            return Promise.resolve('{"name":"john","birthdate":"1936-03-01T00:00:00.000Z"}');
-          }
+          body: '{"name":"john"}'
         });
-        const fetch = fetchWrap(mockedFetch, [
-          middleware.receiveJSON()
-        ]);
-        return fetch('http://localhost/users').then(function(result) {
-          expect(result).to.eql({
-            name: 'john',
-            birthdate: new Date(Date.UTC(1936, 2, 1))
-          });
-        });
-      });
-      it('fails on http errors', function() {
-        const mockedFetch = mockFetch({
-          ok: false,
-          status: 403,
-          statusText: 'Not Authorized'
-        });
-        const fetch = fetchWrap(mockedFetch, [
-          middleware.receiveJSON()
-        ]);
-        return expect(fetch('http://localhost/users')).to.eventually
-          .be.rejectedWith('http error 403: Not Authorized');
       });
     });
+  });
 
-    describe('#testing', function() {
-      it('fails if not mocked', function() {
-        const mockedFetch = mockFetch(123);
-        const fetch = fetchWrap(mockedFetch, [
-          middleware.testing({
-          })
-        ]);
-        return expect(fetch('http://localhost/users', { method: 'PUT' }))
-          .to.eventually.be.rejectedWith('[fetch] request mock not found for: http://localhost/users');
+  describe('#receiveJSON', function() {
+    it('receives JSON responses', function() {
+      const mockedFetch = mockFetch({
+        ok: true,
+        headers: {
+          get: function(name) {
+            if (name === 'content-type') {
+              return 'application/json';
+            }
+          }
+        },
+        text: function() {
+          return Promise.resolve('{"name":"john","birthdate":"1936-03-01T00:00:00.000Z"}');
+        }
       });
-      it('can mock the response', function() {
-        const mockedFetch = mockFetch(123);
-        const fetch = fetchWrap(mockedFetch, [
-          middleware.testing({
-            mock: 321
-          })
-        ]);
-        return fetch('http://localhost/users', { method: 'PUT' })
-        .then(function(result) {
-          expect(result).to.eql(321);
+      const fetch = fetchWrap(mockedFetch, [
+        middleware.receiveJSON()
+      ]);
+      return fetch('http://localhost/users').then(function(result) {
+        expect(result).to.eql({
+          name: 'john',
+          birthdate: new Date(Date.UTC(1936, 2, 1))
         });
       });
-      it('can spy requests', function() {
-        const mockedFetch = mockFetch(123);
-        const spyLog = [];
-        const fetch = fetchWrap(mockedFetch, [
-          middleware.testing({
-            spy: function() {
-              spyLog.push(Array.prototype.slice.apply(arguments));
-            },
-            mock: 321
-          })
+    });
+    it('fails on http errors', function() {
+      const mockedFetch = mockFetch({
+        ok: false,
+        status: 403,
+        statusText: 'Not Authorized'
+      });
+      const fetch = fetchWrap(mockedFetch, [
+        middleware.receiveJSON()
+      ]);
+      return expect(fetch('http://localhost/users')).to.eventually
+        .be.rejectedWith('http error 403: Not Authorized');
+    });
+  });
+
+  describe('#testing', function() {
+    it('fails if not mocked', function() {
+      const mockedFetch = mockFetch(123);
+      const fetch = fetchWrap(mockedFetch, [
+        middleware.testing({
+        })
+      ]);
+      return expect(fetch('http://localhost/users', { method: 'PUT' }))
+        .to.eventually.be.rejectedWith('[fetch] request mock not found for: http://localhost/users');
+    });
+    it('can mock the response', function() {
+      const mockedFetch = mockFetch(123);
+      const fetch = fetchWrap(mockedFetch, [
+        middleware.testing({
+          mock: 321
+        })
+      ]);
+      return fetch('http://localhost/users', { method: 'PUT' })
+      .then(function(result) {
+        expect(result).to.eql(321);
+      });
+    });
+    it('can spy requests', function() {
+      const mockedFetch = mockFetch(123);
+      const spyLog = [];
+      const fetch = fetchWrap(mockedFetch, [
+        middleware.testing({
+          spy: function() {
+            spyLog.push(Array.prototype.slice.apply(arguments));
+          },
+          mock: 321
+        })
+      ]);
+      return fetch('http://localhost/users', { method: 'PUT' })
+      .then(function(result) {
+        expect(spyLog).to.eql([
+          ['http://localhost/users', { method: 'PUT' }]
         ]);
-        return fetch('http://localhost/users', { method: 'PUT' })
-        .then(function(result) {
-          expect(spyLog).to.eql([
-            ['http://localhost/users', { method: 'PUT' }]
-          ]);
-        });
       });
     });
   });
